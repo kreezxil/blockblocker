4fc210db251eeeed40ed88784754bbcef658648e
Eleksploded <pikachu9234@gmail.com>
2019-06-23 15:46:28 -0700

    1.14

--- src/main/java/com/kreezcraft/blockblocker/BlockBlocker.java
+++ src/main/java/com/kreezcraft/blockblocker/BlockBlocker.java
@@ -1,54 +1,18 @@
 package com.kreezcraft.blockblocker;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.config.ModConfig;
 
-@Mod(modid = BlockBlocker.MODID,
-        version = BlockBlocker.VERSION,
-        name = BlockBlocker.NAME,
-        acceptableRemoteVersions = "*")
-public class BlockBlocker {
-    public static final String MODID = "blockblocker";
-    public static final String NAME = "No Op Spawn Protection";
-    public static final String VERSION = "@VERSION@";
-
-    @SidedProxy(clientSide = "com.kreezcraft.blockblocker.ClientProxy", serverSide = "com.kreezcraft.blockblocker.ServerProxy")
-    public static CommonProxy proxy;
-
-    @Mod.Instance
-    public static BlockBlocker instance;
-
-    public static Logger logger;
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
-        proxy.preInit(event);
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("blockblocker")
+public class BlockBlocker
+{
+    public BlockBlocker() {
+        //MinecraftForge.EVENT_BUS.register(this);
+    	ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.spec);
+        MinecraftForge.EVENT_BUS.register(new BlockingHandler());
     }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent e) {
-        proxy.init(e);
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) {
-        proxy.postInit(e);
-    }
-
-// the commands work they just don't appear to modify the config file like they should
-    // or I'm getting confused, gonna release what does work tho
-    // I really don't want the commands in place until I can sync the config to the client
-//    @Mod.EventHandler
-//    public void serverLoad(FMLServerStartingEvent event) {
-//    	event.registerServerCommand(new CommandSpawnProtect());
-//    	event.registerServerCommand(new CommandAllow());
-//    	event.registerServerCommand(new CommandDimension());
-//    	event.registerServerCommand(new CommandRadius());
-//    }
-
 }
+ 
--- src/main/java/com/kreezcraft/blockblocker/Config.java
+++ src/main/java/com/kreezcraft/blockblocker/Config.java
@@ -1,41 +1,42 @@
 package com.kreezcraft.blockblocker;
 
-import net.minecraftforge.common.config.Configuration;
-import org.apache.logging.log4j.Level;
+import java.util.Arrays;
+import java.util.List;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class Config {
 
-    private static final String CATEGORY_PLACEMENT = "no-placement";
-    private static final String CATEGORY_HARVEST = "no-harvest";
-    private static final String CATEGORY_INTERACT = "no-interact";
-    public static Configuration cfg = ServerProxy.config;
-    public static String[] dontPlace;
-    public static String[] dontHarvest;
-    public static String[] dontInteract;
+	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final General GENERAL = new General(BUILDER);
 
-    public static void readConfig() {
-        try {
-            cfg.load();
-            initGeneralConfig(cfg);
-        } catch (Exception e1) {
-            BlockBlocker.logger.log(Level.ERROR, "Problem loading config file!", e1);
-        } finally {
-            if (cfg.hasChanged()) {
-                cfg.save();
-            }
+    public static class General {
+        public final ConfigValue<List<? extends String>> dontInteract;
+        public final ConfigValue<List<? extends String>> dontHarvest;
+        public final ConfigValue<List<? extends String>> dontPlace;
+
+        public General(ForgeConfigSpec.Builder builder) {
+        	
+        	List<String> defaultValues = Arrays.asList(new String[]{"minecraft:bedrock", "minecraft:air"});
+        	
+            builder.push("General");
+            dontInteract = builder
+                    .comment("Prevent these BLOCKS from being interacted with entirely (Only works if configured on client as well as server)")
+                    .translation("bbconfig.interact")
+                    .define("REE", defaultValues);
+            dontHarvest = builder
+            		.comment("BLOCK id's in this list will not be harvestable")
+            		.translation("bbconfig.harvest")
+            		.define("blacklist", defaultValues);
+            dontPlace = builder
+            		.comment("BLOCK id's in this list will not be placeable and\nwill not spawn in the world nor return to the player")
+            		.translation("bbconfig.placement")
+            		.define("randomBed", defaultValues);
+            builder.pop();
         }
+        
     }
-
-    private static void initGeneralConfig(Configuration cfg) {
-
-        cfg.addCustomCategoryComment(CATEGORY_PLACEMENT, "Prevent these BLOCKS from placing. Be sure to include metadata in the Block Ids. Format should be \"modid:block/metadata\"");
-        dontPlace = cfg.getStringList("dontPlace", CATEGORY_PLACEMENT, new String[]{"minecraft:bedrock/0", "minecraft:air/0"}, "BLOCK id's in this list will not be placeable and\nwill not spawn in the world nor return to the player");
-
-        cfg.addCustomCategoryComment(CATEGORY_HARVEST, "Prevent these BLOCKS from be harvested. Be sure to include metadata in the Block Ids. Format should be \"modid:block/metadata\"");
-        dontHarvest = cfg.getStringList("dontHarvest", CATEGORY_HARVEST, new String[]{"minecraft:bedrock/0", "minecraft:air/0"}, "BLOCK id's in this list will not be harvestable");
-
-        cfg.addCustomCategoryComment(CATEGORY_INTERACT, "Prevent these BLOCKS from being interacted with entirely (Only works if configured on client as well as server). Be sure to include metadata in the Block Ids. Format should be \"modid:block/metadata\"");
-        dontInteract = cfg.getStringList("dontInteract", CATEGORY_INTERACT, new String[]{"minecraft:bedrock/0", "minecraft:air/0"}, "BLOCK id's in this list will not be right clickable");
-    }
+    public static final ForgeConfigSpec spec = BUILDER.build();
 
 }
